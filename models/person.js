d07d0593cce459f08cbce68a4b9ac3bbca368363
jussitus <juzzitus@protonmail.com>
2024-05-16 22:16:09 +0300

    ex 3.22

--- models/person.js
+++ models/person.js
@@ -9,34 +9,34 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose.connect(url)
 
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    id: Number,
-    name: {
-        type: String,
-        minlength: 3,
-    },
-    number: {
-        type: String,
-        validate: {
-            validator: v => /^(\d{2}-\d{6,})$|^(\d{3}-\d{5,})$/.test(v),
-            message: () => "The number must start with 2 or 3 digits, be separated with a dash and be at least 8 digits in length. Example: 040-12345"
-        }
+  id: Number,
+  name: {
+    type: String,
+    minlength: 3,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: v => /^(\d{2}-\d{6,})$|^(\d{3}-\d{5,})$/.test(v),
+      message: () => 'The number must start with 2 or 3 digits, be separated with a dash and be at least 8 digits in length. Example: 040-12345'
     }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 
